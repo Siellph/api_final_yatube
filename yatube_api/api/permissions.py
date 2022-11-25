9c761bf09fff2946f231a3a5216ca9f38a49f953
Vladislav Grodin <gordin@it-impuls.ru>
2022-11-25 22:01:37 +0500

    sprint 9

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -7,7 +7,3 @@ class AuthorOrReadOnly(permissions.BasePermission):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated
                 )
-
-    def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user)
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,14 +1,11 @@
-from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
-from posts.models import Follow, Group, Post
+from posts.models import Group, Post
 from rest_framework import exceptions, filters, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
 from .permissions import AuthorOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
                           PostSerializer)
-
-User = get_user_model()
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -58,16 +55,13 @@ class CommentViewSet(viewsets.ModelViewSet):
 
 
 class FollowViewSet(viewsets.ModelViewSet):
-    queryset = Follow.objects.all()
     serializer_class = FollowSerializer
     filter_backends = (filters.SearchFilter, )
     search_fields = ('following__username',)
 
     def get_queryset(self):
-        user = get_object_or_404(User, username=self.request.user)
-        new_queryset = user.follower.all()
-        return new_queryset
+        user = self.request.user
+        return user.follower.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-        super(FollowViewSet, self).perform_create(serializer)
