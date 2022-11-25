63aec0f1d3099d0bf0a5d4a642aa3f92b7dcd9dd
Vladislav <vl241095@gmail.com>
2022-11-25 11:59:31 +0500

    sprint 9

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# api_final
-api final
+## Учебный проект API для сервиса блогов YATUBE
+API для сервиса блогов Yatube через который можно оставлять и просматривать публикации.
+Подписываться на других пользователей.
+Получать:
+записи групп
+информацию об авторе
+детальную информацию о посте
+
+## Установка
+На локальном ПК:
+1. создать виртуальное окружение и активировать его
+[python -m venv venv]
+[source venv/scripts/activate]
+2. установить все зависимости
+[pip install -r requirements.txt]
+3. произвести миграцию БД
+[python manage.py migrate]
+4. запустить сервер
+[python manage.py migrate]
+
+## Примеры запросов к API
+Получить все записи
+[GET] http://127.0.0.1:8000/api/v1/posts/
+Results
+{
+"id": 0,
+"author": "string",
+"text": "string",
+"pub_date": "2021-10-14T20:41:29.648Z",
+"image": "string",
+"group": 0
+}
+
+Создать запись
+[POST] http://127.0.0.1:8000/api/v1/posts/
+Body
+{
+"text": "string",
+"image": "string",
+"group": 0
+}
+Response
+200:
+{
+"id": 0,
+"author": "string",
+"text": "string",
+"pub_date": "2019-08-24T14:15:22Z",
+"image": "string",
+"group": 0
+}
+400:
+{
+"text": [
+"Обязательное поле."
+]
+}
+401:
+{
+"detail": "Учетные данные не были предоставлены."
+}
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,73 @@
-# TODO:  Напишите свой вариант
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from posts.models import Follow, Group, Post
+from rest_framework import exceptions, filters, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+
+from .permissions import AuthorOrReadOnly
+from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
+                          PostSerializer)
+
+User = get_user_model()
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (AuthorOrReadOnly,)
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (AuthorOrReadOnly,)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (AuthorOrReadOnly,)
+
+    def get_queryset(self):
+        post_id = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        new_queryset = post_id.comments.all()
+        return new_queryset
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        serializer.save(
+            author=self.request.user,
+            post=post
+        )
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise exceptions.PermissionDenied('Изменение чужого контента '
+                                              'запрещено!')
+        super(CommentViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise exceptions.PermissionDenied(
+                'Изменение чужого контента запрещено!'
+            )
+        super(CommentViewSet, self).perform_destroy(instance)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.request.user)
+        new_queryset = user.follower.all()
+        return new_queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        super(FollowViewSet, self).perform_create(serializer)
