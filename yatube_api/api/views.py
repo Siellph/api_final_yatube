e41d407d7f64a908fc8ed9604be9d581c7eec8fa
Vladislav <vl241095@gmail.com>
2022-11-28 12:39:50 +0500

    sprint 9. исправления

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
-from posts.models import Group, Post
-from rest_framework import exceptions, filters, viewsets
+from posts.models import Follow, Group, Post
+from rest_framework import filters, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
 from .permissions import AuthorOrReadOnly
@@ -28,33 +28,18 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (AuthorOrReadOnly,)
 
-    def get_queryset(self):
-        post_id = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
-        new_queryset = post_id.comments.all()
-        return new_queryset
-
     def perform_create(self, serializer):
-        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
-        serializer.save(
-            author=self.request.user,
-            post=post
-        )
+        get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user)
 
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise exceptions.PermissionDenied('Изменение чужого контента '
-                                              'запрещено!')
-        super(CommentViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise exceptions.PermissionDenied(
-                'Изменение чужого контента запрещено!'
-            )
-        super(CommentViewSet, self).perform_destroy(instance)
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return post.comments.all()
 
 
 class FollowViewSet(viewsets.ModelViewSet):
+    http_method_names = ['get', 'post']
+    queryset = Follow.objects.all()
     serializer_class = FollowSerializer
     filter_backends = (filters.SearchFilter, )
     search_fields = ('following__username',)
@@ -64,4 +49,4 @@ class FollowViewSet(viewsets.ModelViewSet):
         return user.follower.all()
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        return serializer.save(user=self.request.user)
